b09b0dda4e7a86147c503dfc4ef5d6969a91f455
Martin Metodiev <martin@mev.bg>
2025-08-17 16:56:49 +0300

    Add “block” class to SiteLogo link

--- app/core/components/HeaderBar/HeaderBar.vue
+++ app/core/components/HeaderBar/HeaderBar.vue
@@ -1,7 +1,7 @@
 <template>
   <header :class="[$style.root, 'flex items-center justify-between']">
     <LayoutWrapper class="flex w-full items-center justify-between">
-      <nuxt-link to="/">
+      <nuxt-link to="/" class="block">
         <SiteLogo />
       </nuxt-link>
     </LayoutWrapper>
