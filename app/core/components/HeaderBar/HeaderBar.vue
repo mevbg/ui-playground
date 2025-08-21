074b9991883e9418cd444316708187e7199c71ff
Martin Metodiev <martin@mev.bg>
2025-08-21 14:52:28 +0300

    Refine HeaderBar

--- app/core/components/HeaderBar/HeaderBar.vue
+++ app/core/components/HeaderBar/HeaderBar.vue
@@ -1,6 +1,6 @@
 <template>
   <header :class="[$style.root, 'flex items-center justify-between']">
-    <LayoutWrapper class="flex w-full items-center justify-between">
+    <LayoutWrapper class="flex w-full items-center justify-center">
       <nuxt-link to="/" class="block" aria-label="Начална страница">
         <SiteLogo />
       </nuxt-link>
@@ -9,11 +9,14 @@
 </template>
 
 <style module>
-.root {
-  --offset: var(--mev-size-300x400);
+:root {
+  --mev-header-offset: var(--mev-top-offset);
+  /* prettier-ignore */
+  --mev-header-height: calc(var(--mev-site-logo-height) + var(--mev-header-offset) * 2);
+}
 
-  margin-top: calc(var(--offset) * -1);
-  padding-top: var(--offset);
+.root {
+  padding-block: var(--mev-header-offset);
   background: linear-gradient(0deg, transparent 0%, var(--mev-color-background-primary-base) 99%);
   backdrop-filter: blur(10px);
 }
