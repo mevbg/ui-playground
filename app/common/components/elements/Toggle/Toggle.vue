44e932fa022ff11bf486a0dae3c9c567f6b2d697
Martin Metodiev <martin@mev.bg>
2025-08-21 14:51:03 +0300

    Refinie focus logic in Toggle

--- app/common/components/elements/Toggle/Toggle.vue
+++ app/common/components/elements/Toggle/Toggle.vue
@@ -66,7 +66,7 @@ const classList = computed(() => {
   --stage-width: calc(var(--mev-dimensions-toggle-thumb) * 2 + var(--thumb-offset) * 2);
   --stage-height: calc(var(--mev-dimensions-toggle-thumb) + var(--thumb-offset) * 2);
 
-  &:focus-within {
+  &:has(input:focus-visible) {
     outline: 2px solid var(--mev-color-primitive-red50);
     outline-offset: 3px;
   }
